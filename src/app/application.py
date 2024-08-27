dc9ec3a82c0dbe7c0b779b911692b1e065b6e36c
JamahaW <pret2titio1n@gmail.com>
2024-08-27 04:27:00 +0300

    abc figure work field

--- src/app/application.py
+++ src/app/application.py
@@ -2,151 +2,15 @@ from __future__ import annotations
 
 import math
 from pathlib import Path
-from typing import Iterable
 
 from dearpygui import dearpygui as dpg
 
-from app.ui.abc import ItemID
-from app.ui.custom.widgets import Border
-from app.ui.custom.widgets import SpinboxInt
-from app.ui.dpg.impl import Axis
 from app.ui.dpg.impl import Button
-from app.ui.dpg.impl import Checkbox
-from app.ui.dpg.impl import DragPoint
 from app.ui.dpg.impl import FileDialog
 from app.ui.dpg.impl import Group
-from app.ui.dpg.impl import LineSeries
-from app.ui.dpg.impl import Plot
-from app.ui.dpg.impl import Text
-
-
-class FigureStatusText(Text):
-
-    @staticmethod
-    def __getString(position: tuple[float, float], size: tuple[float, float]) -> str:
-        px, py = position
-        sx, sy = size
-        return f"Position: {px:.1f}x{py:.1f}, Size: {sx:.1f}x{sy:.1f}"
-
-    def update(self, position: tuple[float, float], size: tuple[float, float]) -> None:
-        self.setValue(self.__getString(position, size))
-
-    def placeRaw(self, parent_id: ItemID) -> None:
-        super().placeRaw(parent_id)
-        self.update((0, 0), (0, 0))
-
-
-class FigureSeries(LineSeries):
-
-    def __init__(self, vertices: tuple[Iterable[float], Iterable[float]], label: str) -> None:
-        super().__init__(label)
-        self.__source_vertices_x, self.__source_vertices_y = vertices
-        self.__size = (100, 100)
-        self.__sin_angle: float = 0
-        self.__cos_angle: float = 0
-        self.setRotation(0)
-
-        self.__position_point = DragPoint(self.__onPositionChanged, label="Position")
-        self.__size_point = DragPoint(self.__onSizeChanged, label="Size", default_value=self.__size)
-        self.__set_controls_visible_checkbox = Checkbox(self.__onSetControlsVisibleChanged, label="Controls Visible", default_value=True)
-        self.__status_text = FigureStatusText()
-
-    def getPosition(self) -> tuple[float, float]:
-        return self.__position_point.getValue()
-
-    def setRotation(self, angle: int) -> None:
-        angle = math.radians(angle)
-        self.__sin_angle = math.sin(angle)
-        self.__cos_angle = math.cos(angle)
-
-    def setPosition(self, position: tuple[float, float]) -> None:
-        self.__position_point.setValue(position)
-
-    def setSize(self, size: tuple[float, float]) -> None:
-        position_x, position_y = self.getPosition()
-        self.__size = size_x, size_y = size
-        self.__size_point.setValue((position_x + size_x, position_y + size_y))
-
-    def getSize(self) -> tuple[float, float]:
-        return self.__size
-
-    def getTransformedVertices(self) -> tuple[list[float], list[float]]:
-        transformed_x = list[float]()
-        transformed_y = list[float]()
-
-        size_x, size_y = self.getSize()
-        position_x, position_y = self.getPosition()
-
-        sin_angle = self.__sin_angle
-        cos_angle = self.__cos_angle
-
-        for x, y in zip(self.__source_vertices_x, self.__source_vertices_y):
-            x *= size_x
-            y *= size_y
-
-            transformed_x.append(cos_angle * x - sin_angle * y + position_x)
-            transformed_y.append(sin_angle * x + cos_angle * y + position_y)
-
-        return transformed_x, transformed_y
-
-    def attachIntoCanvas(self, canvas: Canvas) -> None:
-        canvas.add(self.__position_point)
-        canvas.add(self.__size_point)
-        canvas.axis.add(self)
-        self.update()
-
-    def update(self) -> None:
-        self.setValue(self.getTransformedVertices())
-
-    def delete(self) -> None:
-        super().delete()
-        self.__position_point.delete()
-        self.__size_point.delete()
-
-    def placeRaw(self, parent_id: ItemID) -> None:
-        super().placeRaw(parent_id)
-        self.add(self.__status_text)
-        self.add(SpinboxInt("rotation", (0, 360), self.__onRotationChanged, step=15, default_value=0))
-        self.add(self.__set_controls_visible_checkbox)
-        self.add(Button("[Remove]", self.delete))
-
-    def __onPositionChanged(self, new_position: tuple[float, float]) -> None:
-        position_x, position_y = new_position
-        scale_x, scale_y = self.__size
-        self.__size_point.setValue((position_x + scale_x, position_y + scale_y))
-        self.update()
-        self.__status_text.update(new_position, self.__size)
-
-    def __onSizeChanged(self, new_scale: tuple[float, float]) -> None:
-        scale_x, scale_y = new_scale
-        position_x, position_y = position = self.getPosition()
-        self.__size = (scale_x - position_x, scale_y - position_y)
-        self.update()
-        self.__status_text.update(position, new_scale)
-
-    def __onRotationChanged(self, new_rotation) -> None:
-        self.setRotation(new_rotation)
-        self.update()
-
-    def __onSetControlsVisibleChanged(self, is_visible: bool) -> None:
-        self.__position_point.setVisible(is_visible)
-        self.__size_point.setVisible(is_visible)
-
-
-class Canvas(Plot):
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.axis = Axis(dpg.mvXAxis)
-        self.border = Border(50)
-
-    def placeRaw(self, parent_id: ItemID) -> None:
-        super().placeRaw(parent_id)
-        self.add(self.axis)
-        self.add(self.border)
-
-    def attachFigure(self, figure: FigureSeries) -> None:
-        figure.attachIntoCanvas(self)
+from app.ui.plotter.figure import Canvas
+from app.ui.plotter.figure import TransformableFigure
+from app.ui.plotter.figure import WorkFieldFigure
 
 
 class App:
@@ -163,15 +27,14 @@ class App:
         self.items_count = 0
 
     def addCircleItem(self) -> None:
-        self.items_count += 1
-
         R = range(0, 271, 1)
         vertices = (
             [math.cos(math.radians(i)) for i in R],
             [math.sin(math.radians(i)) for i in R]
         )
 
-        circle = FigureSeries(vertices, "Figure: Circle-Test")
+        circle = TransformableFigure(vertices, f"Figure: Test:{self.items_count}")
+        self.items_count += 1
 
         self.canvas.attachFigure(circle)
 
@@ -188,7 +51,9 @@ class App:
 
                 self.canvas.place()
 
-        self.canvas.border.setSize(1200, 1200)
+        work_field_figure = WorkFieldFigure("Work Field")
+        self.canvas.attachFigure(work_field_figure)
+        work_field_figure.setSize((1000, 1000))
 
 
 def start_application(app_title: str, window_width: int, window_height: int) -> None:
--- src/app/ui/custom/widgets.py
+++ src/app/ui/custom/widgets.py
@@ -10,7 +10,20 @@ from app.ui.dpg.impl import SliderInt
 from app.ui.dpg.impl import Text
 
 
-class BorderLinePair(Placeable):
+class BorderLinePair(Placeable, VariableItem[float]):
+
+    def __init__(self, is_vertical: bool, on_change: Callable[[float], None] = None, *, step: int = 1) -> None:
+        self.__positive_line = DragLine(is_vertical, self.__setHalfSize)
+        self.__negative_line = DragLine(is_vertical, self.__setHalfSize)
+        self.__on_change = on_change
+        self.step = step
+
+    def setValue(self, size: float) -> None:
+        self.__on_change(size)
+        self.__setHalfSize(size / 2)
+
+    def getValue(self) -> float:
+        return self.__positive_line.getValue() * 2
 
     def hide(self) -> None:
         self.__positive_line.hide()
@@ -35,11 +48,6 @@ class BorderLinePair(Placeable):
         self.__positive_line.show()
         self.__negative_line.show()
 
-    def __init__(self, is_vertical: bool, step) -> None:
-        self.__positive_line = DragLine(is_vertical, self.__setHalfSize)
-        self.__negative_line = DragLine(is_vertical, self.__setHalfSize)
-        self.step = step
-
     def placeRaw(self, parent_id: ItemID) -> None:
         self.__positive_line.placeRaw(parent_id)
         self.__negative_line.placeRaw(parent_id)
@@ -49,14 +57,29 @@ class BorderLinePair(Placeable):
         self.__positive_line.setValue(half_size)
         self.__negative_line.setValue(-half_size)
 
-    def setSize(self, size: float) -> None:
-        self.__setHalfSize(size / 2)
-
-    def getSize(self) -> float:
-        return self.__positive_line.getValue() * 2
+        if self.__on_change:
+            self.__on_change(half_size * 2)
 
 
-class Border(Placeable):
+class Border(Placeable, VariableItem[tuple[float, float]]):
+
+    def __init__(self, on_change: Callable[[tuple[float, float]], None] = None, *, step: int) -> None:
+        on_width_changed = None if on_change is None else lambda width: on_change((width, self.__height_lines.getValue()))
+        on_height_changed = None if on_change is None else lambda height: on_change((self.__width_lines.getValue(), height))
+        self.__on_change = on_change
+        self.__width_lines = BorderLinePair(True, on_width_changed, step=step)
+        self.__height_lines = BorderLinePair(False, on_height_changed, step=step)
+
+    def setValue(self, size: tuple[float, float]) -> None:
+        width, height = size
+        self.__width_lines.setValue(width)
+        self.__height_lines.setValue(height)
+
+        if self.__on_change:
+            self.__on_change(size)
+
+    def getValue(self) -> tuple[float, float]:
+        return self.__width_lines.getValue(), self.__height_lines.getValue()
 
     def show(self) -> None:
         self.__width_lines.show()
@@ -81,20 +104,9 @@ class Border(Placeable):
         self.__width_lines.delete()
         self.__height_lines.delete()
 
-    def __init__(self, step: int) -> None:
-        self.__width_lines = BorderLinePair(False, step)
-        self.__height_lines = BorderLinePair(True, step)
-
     def placeRaw(self, parent_id: ItemID) -> None:
         self.__width_lines.placeRaw(parent_id)
         self.__height_lines.placeRaw(parent_id)
-
-    def setSize(self, width: int, height: int) -> None:
-        self.__width_lines.setSize(width)
-        self.__height_lines.setSize(height)
-
-    def getSize(self) -> tuple[float, float]:
-        return self.__width_lines.getSize(), self.__height_lines.getSize()
 
 
 class SpinboxInt(VariableItem[int], Group):
